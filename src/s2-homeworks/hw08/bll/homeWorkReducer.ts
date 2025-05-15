b46ae0b0bac0ba6234c6f19d95bd6328813de7c0
Michael Dudzik <mkdudik15@gmail.com>
2025-05-15 23:32:26 +0300

    create homeWorkReducer

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,18 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer  = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+          return  [...state] // создаем новый массив (иммутабельность)
+                .sort((a, b) => action.payload === 'up'
+                    ? a.name.localeCompare(b.name)
+                    : b.name.localeCompare(a.name)); // need to fix
         }
         case 'check': {
+            return state.filter(person => person.age >= action.payload)
 
-            return state // need to fix
+          // need to fix
         }
         default:
             return state
